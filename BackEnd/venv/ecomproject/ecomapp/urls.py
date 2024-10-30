3aac0f168cbb9ca3501499a252deca287bd52953
Nora Idris <noraidris621@gmail.com>
2024-10-30 14:12:36 +0100

    get product request (id,pk)

--- BackEnd/venv/ecomproject/ecomapp/urls.py
+++ BackEnd/venv/ecomproject/ecomapp/urls.py
@@ -1,12 +1,10 @@
 from ecomapp import views 
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.getRoutes, name="getRoutes"),
     path('products/', views.getProducts, name="getProducts"),
+    path('product/<str:pk>', views.getProduct, name="getProduct"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
--- BackEnd/venv/ecomproject/ecomapp/views.py
+++ BackEnd/venv/ecomproject/ecomapp/views.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Products
 from .serializers import ProductsSerializers
-import logging
 
-logger = logging.getLogger(__name__)
+
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -16,6 +16,9 @@ def getProducts(request):
         products = Products.objects.all()
         serializers = ProductsSerializers(products, many=True)
         return Response(serializers.data)
-    # except Exception as e:
-    #     logger.error(f"Error fetching products: {e}")
-    #     return Response({"error": "Internal Server Error"}, status=500)
+ 
+@api_view(['GET'])
+def getProduct(request, pk):
+        product = Products.objects.get(_id=pk)
+        serializers = ProductsSerializers(product, many=False)
+        return Response(serializers.data)
